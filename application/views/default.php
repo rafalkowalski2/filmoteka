99d56c0dd63e8008134cc6fac933db5e671c7e46
rafalkowalski2 <rafalkowalski2@outlook.com>
2015-05-21 23:32:40 +0200

    usunięcie zmienej test

--- application/views/default.php
+++ application/views/default.php
@@ -114,7 +114,6 @@
     			</div>
     		</div>
     		<?php echo View::factory('profiler/stats'); ?>
-    		<?php $test ;?>
    		</footer>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="http://<?php echo $server;?>/js/bootstrap.min.js"></script>
