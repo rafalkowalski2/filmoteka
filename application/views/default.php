ad0b2ac1e86e04d255a08831e7cc92504f16eda8
rafalkowalski2 <rafalkowalski2@outlook.com>
2015-05-21 23:29:05 +0200

    test

--- application/views/default.php
+++ application/views/default.php
@@ -114,6 +114,7 @@
     			</div>
     		</div>
     		<?php echo View::factory('profiler/stats'); ?>
+    		<?php $test ;?>
    		</footer>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="http://<?php echo $server;?>/js/bootstrap.min.js"></script>
